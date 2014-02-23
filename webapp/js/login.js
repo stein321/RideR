d6025302297496a25db32c5ba17d578fc2d65698
David Dawson <djdawson3@gmail.com>
2014-02-22 20:10:25 -0800

    yolo

--- webapp/js/login.js
+++ webapp/js/login.js
@@ -59,7 +59,7 @@ var login = (function() {
 			var js, fjs = d.getElementsByTagName(s)[0];
 			if (d.getElementById(id)) {return;}
 			js = d.createElement(s); js.id = id;
-			js.src = "//connect.facebook.net/en_US/all.js";
+			js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=436860186416914";
 			fjs.parentNode.insertBefore(js, fjs);
 		}(document, 'script', 'facebook-jssdk'));
 	})
