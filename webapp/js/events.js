1481f6f2e978f331293a000947cf2593b932d38b
David Dawson <djdawson3@gmail.com>
2014-02-23 13:55:15 -0800

    adding rides page

--- webapp/js/events.js
+++ webapp/js/events.js
@@ -2,7 +2,7 @@ var login = (function() {
 
 	$(document).ready(function() {
 		$("button").click(function() {
-			windlow.location = "listfriends.html";
+			window.location = "listfriends.html";
 		})
 	})
 }());
