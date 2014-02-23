c03079b6a2cb7cf573c1dd67a0d83599b08516a0
Ben Stein <stein321@gmail.com>
2014-02-22 19:28:30 -0800

    Login using facebook button

---
+++ facebook.js
@@ -0,0 +1,7 @@
+
+var friendsGoingToEvent=new Array();
+friendsGoingToEvent[0]={name: "Matt",location:"98101", method:"driving}";
+friendsGoingToEvent[1]={name: "Joe",location:"98040",method:"looking"};
+friendsGoingToEvent[2]={name: "Sam",location: "Capitol Hill", method:"driving"};
+friendsGoingToEvent[3]={name: "Jeff", location: "Redmond", method:"driving"};
+friendsGoingToEvent[4]={name: "Bill", location: "", method: "looking"};
--- index.php
+++ index.php
@@ -1,48 +1,93 @@
-<?php
-  $mysqli = new mysqli("localhost", "root", "n2m9WZW9AjmY", "RideR");
-  if (mysqli_connect_errno()) {
-    printf("Connect failed: %s\n", mysqli_connect_error());
-    exit();
-}
-//pull events from facebook
+<html>
+<head></head>
+<body>
+<div id="fb-root"></div>
+<script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
+<script src="http://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+<script>
+  function sendResponseToServer(response) {
+      console.log(response);
+      $.ajax({
 
-//store ids
+          url: "http://localhost/~Ben/RideR/RideRServer",
+          type: "GET",
+          data: response,
+          context: document.body,
+          success: displayShit
+        });
+  }
+  function displayShit(data) {
+      console.log(data);
+  }
+  function fail() {
+    console.log("uh oh");
+  }
 
-//compare ids
+  window.fbAsyncInit = function() {
+  FB.init({
+    appId      : "436860186416914",
+    status     : true, // check login status
+    cookie     : true, // enable cookies to allow the server to access the session
+    xfbml      : true  // parse XFBML
+  });
 
-?>
+  // Here we subscribe to the auth.authResponseChange JavaScript event. This event is fired
+  // for any authentication related change, such as login, logout or session refresh. This means that
+  // whenever someone who was previously logged out tries to log in again, the correct case below 
+  // will be handled. 
+  FB.Event.subscribe('auth.authResponseChange', function(response) {
+    // Here we specify what we do with the response anytime this event occurs. 
+    if (response.status === 'connected') {
+      // The response object is returned with a status field that lets the app know the current
+      // login status of the person. In this case, we're handling the situation where they 
+      // have logged in to the app.
+      testAPI();
+    } else if (response.status === 'not_authorized') {
+      // In this case, the person is logged into Facebook, but not into the app, so we call
+      // FB.login() to prompt them to do so. 
+      // In real-life usage, you wouldn't want to immediately prompt someone to login 
+      // like this, for two reasons:
+      // (1) JavaScript created popup windows are blocked by most browsers unless they 
+      // result from direct interaction from people using the app (such as a mouse click)
+      // (2) it is a bad experience to be continually prompted to login upon page load.
+      FB.login();
+    } else {
+      // In this case, the person is not logged into Facebook, so we call the login() 
+      // function to prompt them to do so. Note that at this stage there is no indication
+      // of whether they are logged into the app. If they aren't then they'll see the Login
+      // dialog right after they log in to Facebook. 
+      // The same caveats as above apply to the FB.login() call here.
+      FB.login();
+    }
+  });
+  };
 
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap 101 Template</title>
+  // Load the SDK asynchronously
+  (function(d){
+   var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+   if (d.getElementById(id)) {return;}
+   js = d.createElement('script'); js.id = id; js.async = true;
+   js.src = "//connect.facebook.net/en_US/all.js";
+   ref.parentNode.insertBefore(js, ref);
+  }(document));
 
-    <!-- Bootstrap -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-  </head>
+  // Here we run a very simple test of the Graph API after login is successful. 
+  // This testAPI() function is only called in those cases. 
+  function testAPI() {
+    console.log('Welcome!  Fetching your information.... ');
+    FB.api('/me', function(response) {
+      console.log('Good to see you, ' + response.name + '.');
+      // console.log(response.id);
+      sendResponseToServer(response);
+    });
 
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="js/bootstrap.min.js"></script>
-  <body>
-    <div id="fb-root"></div>
-    <script>(function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s); js.id = id;
-    js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
-    fjs.parentNode.insertBefore(js, fjs);
-  }(document, 'script', 'facebook-jssdk'));</script>
-    <h2>
-        RideShare
-    </h2>
-  
-<div class="fb-login-button" data-max-rows="1" data-size="medium" data-show-faces="false" data-auto-logout-link="false"></div>
+  }
+</script>
 
+<!--
+  Below we include the Login Button social plugin. This button uses the JavaScript SDK to
+  present a graphical Login button that triggers the FB.login() function when clicked. -->
 
-  </body>
+<fb:login-button show-faces="true" width="200" max-rows="1"></fb:login-button>
+</body>
 </html>
