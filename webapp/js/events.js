ec7243a87b19ead563be7a9fff670a4a5953b622
David Dawson <djdawson3@gmail.com>
2014-02-22 22:16:17 -0800

    wip fb event querying

--- webapp/js/events.js
+++ webapp/js/events.js
@@ -4,5 +4,9 @@ var login = (function() {
 		$("#eventsSubmit").click(function() {
 			window.location = "/RideR/webapp/rides.html"
 		});
+
+		FB.api('/me/events/attending?since=now', function(response) {
+		  console.log(response);
+		});
 	})
 }());
