dfc452aa2f766e1c2bb7fa1211de8825624735fd
David Dawson <djdawson3@gmail.com>
2014-02-22 23:33:37 -0800

    ewqrqwer

--- webapp/js/events.js
+++ webapp/js/events.js
@@ -1,16 +1,6 @@
 var login = (function() {
 
 	$(document).ready(function() {
-		window.fbAsyncInit = function() {
-			FB.init({
-			  	appId      : '436860186416914',
-			  	channelUrl : "channel.html",
-				status     : true, 
-				cookie     : true,
-				xfbml      : true,
-				oauth      : true,
-			});
-
 			FB.api('/me/events/attending?since=now',
 			{
 				accessToken: FB.getAuthResponse().accessToken
@@ -18,15 +8,6 @@ var login = (function() {
 			function(response) {
 		  		console.log(response);
 			});
-	    }
-
-		(function(d, s, id){
-			var js, fjs = d.getElementsByTagName(s)[0];
-			if (d.getElementById(id)) {return;}
-			js = d.createElement(s); js.id = id;
-			js.src = "//connect.facebook.net/en_US/all.js";
-			fjs.parentNode.insertBefore(js, fjs);
-		}(document, 'script', 'facebook-jssdk'));
 
 		$("#eventsSubmit").click(function() {
 			window.location = "/RideR/webapp/rides.html"
