3efc5090ed29c38021c7fff30d2eb6affdf35ead
David Dawson <djdawson3@gmail.com>
2014-02-23 12:51:06 -0800

    fucking parens

--- webapp/js/login.js
+++ webapp/js/login.js
@@ -3,6 +3,6 @@ var login = (function() {
 	$(document).ready(function() {
 		$("#facebookLoginButton").click(function() {
 			window.location = "/RideR/webapp/location.html";
-		}
+		});
 	});
 }());
