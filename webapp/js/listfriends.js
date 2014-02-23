8216aa5efcd46a635f7e22d88031acdc6c677f42
David Dawson <djdawson3@gmail.com>
2014-02-23 15:05:50 -0800

    stuff

--- webapp/js/listfriends.js
+++ webapp/js/listfriends.js
@@ -1,8 +1,25 @@
 var listfriends = (function() {
 
+	var constructRiderList = function(data) {
+		var div = $('<div/>', {
+			'class':'divRow'
+		});
+	}
+
+	$.ajax({
+    	url: "http://54.201.249.47/RideRServer/",
+     	type: "GET",
+     	context: document.body,
+     	success: constructRiderList
+    });
+
 	$(document).ready(function() {
 		$("#nextButton").click(function() {
 			window.location = "rides.html";
-		})
+		});
+
+		$(".divRow").click(function() {
+			$(this).toggleClass("selected");
+		});
 	})
 }());
