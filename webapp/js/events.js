4dd7f34015e0d5825754e7999a6beef01771660d
David Dawson <djdawson3@gmail.com>
2014-02-22 23:35:59 -0800

    a

--- webapp/js/events.js
+++ webapp/js/events.js
@@ -1,6 +1,7 @@
 var login = (function() {
 
 	$(document).ready(function() {
+		window.fbAsyncInit = function() {
 			FB.api('/me/events/attending?since=now',
 			{
 				accessToken: FB.getAuthResponse().accessToken
@@ -8,6 +9,7 @@ var login = (function() {
 			function(response) {
 		  		console.log(response);
 			});
+	    }
 
 		$("#eventsSubmit").click(function() {
 			window.location = "/RideR/webapp/rides.html"
