bc8430bde5afd15666fb828fa947d223971cc742
David Dawson <djdawson3@gmail.com>
2014-02-22 19:25:05 -0800

    adding facebook button integration

--- webapp/js/login.js
+++ webapp/js/login.js
@@ -4,5 +4,23 @@ var login = (function() {
 		$("#loginButton").click(function() {
 			window.location = "/RideR/webapp/location.html"
 		});
+
+		window.fbAsyncInit = function() {
+			FB.init({
+			  	appId      : '436860186416914',
+				status     : true, 
+				cookie     : true,
+				xfbml      : true,
+				oauth      : true,
+			});
+		};
+
+		(function(d, s, id){
+			var js, fjs = d.getElementsByTagName(s)[0];
+			if (d.getElementById(id)) {return;}
+			js = d.createElement(s); js.id = id;
+			js.src = "//connect.facebook.net/en_US/all.js";
+			fjs.parentNode.insertBefore(js, fjs);
+		}(document, 'script', 'facebook-jssdk'));
 	})
 }());
