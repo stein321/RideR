61b0f71334fdc982aeb13dd3297a3fe9a8a92c85
David Dawson <djdawson3@gmail.com>
2014-02-22 23:30:44 -0800

    ugh

--- webapp/js/events.js
+++ webapp/js/events.js
@@ -20,6 +20,14 @@ var login = (function() {
 			});
 	    }
 
+		(function(d, s, id){
+			var js, fjs = d.getElementsByTagName(s)[0];
+			if (d.getElementById(id)) {return;}
+			js = d.createElement(s); js.id = id;
+			js.src = "//connect.facebook.net/en_US/all.js";
+			fjs.parentNode.insertBefore(js, fjs);
+		}(document, 'script', 'facebook-jssdk'));
+
 		$("#eventsSubmit").click(function() {
 			window.location = "/RideR/webapp/rides.html"
 		});
