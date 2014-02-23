d54c75b07685cf6a5a7b755c3fdea4960100ae62
David Dawson <djdawson3@gmail.com>
2014-02-23 15:40:31 -0800

    asdf

--- webapp/js/login.js
+++ webapp/js/login.js
@@ -8,23 +8,6 @@ var login = (function() {
 		$("fb-root").on("facebook:init", function() {
 			alert("INIT");
 		})
-
-		FB.getLoginStatus(function(response) {
-		  if (response.status === 'connected') {
-		    // the user is logged in and has authenticated your
-		    // app, and response.authResponse supplies
-		    // the user's ID, a valid access token, a signed
-		    // request, and the time the access token 
-		    // and signed request each expire
-		    var uid = response.authResponse.userID;
-		    var accessToken = response.authResponse.accessToken;
-		  } else if (response.status === 'not_authorized') {
-		    // the user is logged in to Facebook, 
-		    // but has not authenticated your app
-		  } else {
-		    // the user isn't logged in to Facebook.
-		  }
-		 });
 	});
 
 }());
