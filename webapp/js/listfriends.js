c275819e48d8eb9da3d96b55b1169e3ba88dc45b
David Dawson <djdawson3@gmail.com>
2014-02-23 15:21:18 -0800

    s

--- webapp/js/listfriends.js
+++ webapp/js/listfriends.js
@@ -1,9 +1,19 @@
 var listfriends = (function() {
 
 	var constructRiderList = function(data) {
-		var div = $('<div/>', {
-			'class':'divRow'
-		});
+		var json = jQuery.parseJSON(data);
+		var i;
+		for(i = 0; i < json.length; i++) {
+			var rider = json[i];
+			var div = $('<div/>', {
+				'class':'divRow'
+			});
+			var nameSpan = $('<span/>')
+			nameSpan.html(rider.name);
+
+			div.html(nameSpan);
+			$("#riderSuggestionContainer").append(div);
+		}
 	}
 
 	$.ajax({
